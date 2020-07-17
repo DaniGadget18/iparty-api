3c0a4220e1bef5628bfadac11c125382b86fa720
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2020-07-16 22:47:12 -0500

    migracion Usuario

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -5,10 +5,15 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
+      this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
+      table.string('nombre', 254).notNullable()
+      table.string('apellidoP', 254).notNullable()
+      table.string('apellidoM', 254)
+      table.string('foto', 254).notNullable()
+      table.date('fecha_nacimiento', 254).notNullable()
       table.string('password', 60).notNullable()
       table.timestamps()
     })
