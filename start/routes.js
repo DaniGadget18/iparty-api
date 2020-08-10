1b6245b239a8f971d88a545b1877d7b0f1202797
DaniGadget18 <dani_181298@hotmail.com>
2020-08-10 13:38:34 -0500

    cambios de reservaciones

--- start/routes.js
+++ start/routes.js
@@ -36,6 +36,7 @@ Route.group( () => {
   Route.post('obtenerNegociobyid', 'RootAdministradorController.obtenerNegocioByID');
   Route.post('registrarNegocio', 'RootAdministradorController.registrarNegocioLucid');
   Route.post('registrarRoot', 'RootAdministradorController.registrarRoot');
+  Route.post('existeNegocio', 'NegocioController.existeNegocio');
 
 
 }).prefix('api/root/')
@@ -44,6 +45,7 @@ Route.group( () => {
 
 Route.group( () => {
   // Negocios
+  Route.post('obteneridnombrenegocio', 'NegocioController.obteneridnombrenegocio');
   Route.post('obtenerNegocio', 'NegocioController.obtenerNegocioByEmail');
   Route.post('editarNegocio', 'NegocioController.updateNegocio');
 
@@ -68,7 +70,13 @@ Route.group( () => {
   // Eventos negocio
   Route.post('obtenerEventos', 'NegocioController.obtenerEventosNegocio');
   Route.post('obtenerEventosFecha', 'NegocioController.obtenerEventosFecha');
+  Route.post('registrarEvento', 'NegocioController.registrarEventoNegocio');
+  Route.post('eliminarEvento', 'NegocioController.eliminarEvento');
+  Route.post('editarEvento', 'NegocioController.editarEvento');
+  Route.post('obtenerEvento', 'NegocioController.obtenerEventoById');
 
+  // Reservaciones negocio
+  Route.post('obtenerReservaciones', 'NegocioController.obtenerReservaciones');
 
   // consultas
   Route.get('top', 'ConsultaController.top');
